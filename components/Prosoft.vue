de1064d652dc209fce4e665e56b940bdc2d58d54
MikeFranco <mfranco_98@yahoo.com>
2019-11-20 21:20:04 -0600

    added Prosoft component

---
+++ components/Prosoft.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <div class="carrousel" >
+      <div>
+        <h1>Carta bajo protesta</h1>
+        <iframe
+          class="pdf"
+          src="https://drive.google.com/file/d/1jPwkrYvtZwCDwlp4PZH2uW87aSRGoenz/view?usp=sharing"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default { };
+</script>
+
+<style scoped>
+
+.pdf {
+  min-width: 880px;
+  max-width: 880px;
+  min-height: 800px;
+  max-height: 800px;
+  margin: 20px;
+}
+
+.carrousel{
+  max-width: 700px;
+  overflow-x: auto;
+  background-color: #005584;
+  margin: 50px;
+}
+
+</style>
--- pages/prosoft/index.vue
+++ pages/prosoft/index.vue
@@ -3,47 +3,19 @@
     <div class="nav-bar">
       <NavBar />
     </div>
-    <div class="carrousel" >
-      <div>
-        <h1>Carta bajo protesta</h1>
-        <iframe
-          class="pdf"
-          src="https://drive.google.com/file/d/1jPwkrYvtZwCDwlp4PZH2uW87aSRGoenz/view?usp=sharing"
-          frameborder="0"
-        ></iframe>
-      </div>
-    </div>
+    <Prosoft />
   </section>
 </template>
 
 <script>
 import NavBar from "~/components/generic-cmpts/NavBar.vue";
+import Prosoft from "~/components/Prosoft";
+
 export default {
-  components: { NavBar },
+  components: { NavBar, Prosoft },
 };
 </script>
 
 <style scoped>
-
-.nav-bar {
-  position: -webkit-sticky; /* Safari */
-  position: sticky;
-  top: 0;
-}
-
-.pdf {
-  min-width: 880px;
-  max-width: 880px;
-  min-height: 800px;
-  max-height: 800px;
-  margin: 20px;
-}
-
-.carrousel{
-  max-width: 700px;
-  overflow-x: auto;
-  background-color: #005584;
-  margin: 50px;
-}
 
 </style>
